2574bc02b0c3159dd100d1ef9964c5d5048aa70f
ponce <aliloko@gmail.com>
2014-02-02 14:31:27 +0100

    Add support for XML, JSON, plain text and msgpack for the "list" command. list command now takes N variable to limit the number of high-scores returned.

--- source/main.d
+++ source/main.d
@@ -16,8 +16,102 @@ class ListScoreHandler : DynamicHttpHandler
     override HttpResponse handle(HttpRequest request, Address remote) 
     {
         HttpResponse response;
-        response.content = cast(ubyte[])(scores.toString());
-        return response;
+        
+        enum Format
+        {
+            TEXT,
+            JSON,
+            XML,
+            MSGPACK
+        }
+
+        try
+        {
+            Format fmt = Format.JSON;
+            int N = scores.count();
+
+            if (request.query != "")
+            {
+                foreach (keyvar; splitter(request.query, "&"))
+                {
+                    auto tok = splitter(keyvar, "=").array;
+                    if (tok.length == 2)
+                    {
+                        if (tok[0] == "format")
+                        {
+                            if (tok[1] == "text")
+                                fmt = Format.TEXT;
+                            else if (tok[1] == "json")
+                                fmt = Format.JSON;
+                            else if (tok[1] == "xml")
+                                fmt = Format.XML;
+                            else if (tok[1] == "msgpack")
+                                fmt = Format.MSGPACK;
+                            else 
+                                throw new Exception(format("Unknown format %s. Accepted values: 'text', 'json', 'xml', 'msgpack'."));
+                        }
+                        else if (tok[0] == "N")
+                        {
+                            N = to!int(tok[1]);
+                        }
+                        else 
+                            throw new Exception(format("Unknown query variable %s. Accepted values are 'format' and 'N'.", tok[0]));
+                    }
+                    else 
+                        throw new Exception(format("Coulnd't parse parameters."));
+                }
+            }
+
+            final switch(fmt)
+            {
+                case Format.TEXT:
+                    string res = "";
+                    for (int i = 0; i < N; ++i)
+                    {
+                        res = res ~ format("%s\n%s\n", scores.data[i].name, scores.data[i].score);
+                    }
+                    response.content = cast(ubyte[])(res);
+                    response.setHeader("Content-Type", "text/plain");
+                    break;
+
+                case Format.JSON:
+                    string res = "{ \"scores\": [ \n";
+                    for (int i = 0; i < N; ++i)
+                    {
+                        res ~= format("    { \"name\": \"%s\", \"score\": %s },\n", scores.data[i].name, scores.data[i].score);
+                    }
+                    res ~= "]};\n";
+                    response.content = cast(ubyte[])(res);
+                    response.setHeader("Content-Type", "application/json");
+                    break;
+
+                case Format.XML:
+                    string res = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
+                    res ~= "<scores>\n";
+                    for (int i = 0; i < N; ++i)
+                        res ~= format("    <entry name=\"%s\" score=\"%s\" />\n", scores.data[i].name, scores.data[i].score);
+                    res ~= "</scores>\n";
+                    response.content = cast(ubyte[])(res);
+                    response.setHeader("Content-Type", "text/xml");
+                    break;
+
+                case Format.MSGPACK:
+                    import msgpack;
+                    ubyte[] res = pack(scores.data[0..N]);
+                    response.content = res;
+                    response.setHeader("Content-Type", "application/x-msgpack");
+                    break;
+            }
+
+            response.status = 200;
+            return response;
+        }
+        catch(Exception e)
+        {
+            response.status = 400;
+            response.content = cast(ubyte[])(e.msg);
+            return response;
+        }
     }
 
     Scores scores;
@@ -107,19 +201,7 @@ class Scores
         {
             data ~= new Score("Anonymous", 0);
         }
-    }
-
-    Score[] data;
-
-    override string toString()
-    {
-        string res = "";
-        for (size_t i = 0; i < data.length; ++i)
-        {
-            res = res ~ format("%s\n%s\n", data[i].name, data[i].score);
-        }
-        return res;
-    }
+    }    
 
     void update(Score score)
     {
@@ -127,16 +209,27 @@ class Scores
         data.sort;
         data.length = data.length - 1;
     }
+
+    // number of highscores
+    int count()
+    {
+        return cast(int) data.length;
+    }
+
+    Score[] data;
 }
 
 void main() 
 {
-    auto scores = new Scores(20);
+    auto scores = new Scores(100);
 	addDynamicHandler(new UpdateScoreHandler(scores));	
     addDynamicHandler(new ListScoreHandler(scores));	
 
     ServerSettings settings;
     settings.port = 8080;
+    settings.maxConnections = 100;
+    settings.connectionTimeoutMs = 10000;
+    settings.connectionQueueSize = 30;
 	
 	startServer(settings);
     scope(exit) stopServer();
